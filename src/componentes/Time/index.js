209c365782f26dcf10489a8a0a35a4f5b0e2ab16
Afonso Gouveia <gouvei44@gmail.com>
2023-03-30 16:20:47 -0300

    Add files via upload

---
+++ src/componentes/Time/index.js
@@ -0,0 +1,28 @@
+import Colaborador from '../Colaborador'
+import hexToRgba from 'hex-to-rgba';
+import './time.css'
+
+const Time = ({ time, colaboradores, aoDeletar, mudarCor, aoFavoritar }) => {
+    return (
+        colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: hexToRgba(time.cor, '0.6')}}>
+            <input onChange={evento => mudarCor(evento.target.value, time.id)} value={time.cor} type='color' className='input-color' />
+            <h3 style={{ borderColor: time.cor }}>{time.nome}</h3>
+            <div className='colaboradores'>
+                {colaboradores.map((colaborador, indice) => {
+                    return (
+                        <Colaborador 
+                            key={indice} 
+                            colaborador={colaborador} 
+                            corDeFundo={time.cor} 
+                            aoDeletar={aoDeletar}
+                            aoFavoritar={aoFavoritar}
+                        />
+                    );
+                })}
+            </div>
+        </section>
+
+    )
+}
+
+export default Time
